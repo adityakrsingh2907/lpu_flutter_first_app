441309d7a75cf62a5009f183369e55aabb00a172
Aditya Kumar Singh <94990196+anonymouslyfadeditzme@users.noreply.github.com>
2023-01-12 16:21:29 +0530

    first commit

---
+++ first_app/lib/main.dart
@@ -0,0 +1,9 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(MaterialApp(
+      home: Scaffold(
+    body: Center(child: Text("Hello World")),
+    appBar: AppBar(title: Text("My First App")),
+  )));
+}
